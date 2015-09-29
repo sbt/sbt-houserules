c1cacda8c71a24eb08de56c01616c74769477526
Eugene Yokota <eed3si9n@gmail.com>
2015-09-29 00:39:21 -0400

    add scalastyle-sbt-plugin

---
+++ src/main/scala/sbt/houserules/StylePlugin.scala
@@ -0,0 +1,18 @@
+package sbt
+package houserules
+
+import sbt._
+import Keys._
+import Path._
+import org.scalastyle.sbt.ScalastylePlugin._
+
+object StylePlugin extends AutoPlugin {
+  override def requires = plugins.JvmPlugin
+  override def trigger = allRequirements
+
+  override def projectSettings: Seq[Def.Setting[_]] = baseSettings
+
+  lazy val baseSettings: Seq[Setting[_]] = Seq(
+    scalastyleConfig := { new File((baseDirectory in LocalRootProject).value, "scalastyle-config.xml") }
+  )
+}
